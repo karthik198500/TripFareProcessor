75710b7a0d97f2a4142d27021b595f0c90812863
krishna.kasiraju <krishna.kasiraju@five9.com>
2022-03-02 23:00:38 +1100

    First Commit

---
+++ src/main/java/com/lp/tripfareprocessor/TripFareProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.lp.tripfareprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFareProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripFareProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lp/tripfareprocessor/TripFareProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.tripfareprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripFareProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
